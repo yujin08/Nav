dc5017fd58a7b0208957aa0db7a50befca124e6a
yujin08 <97872603+yujin08@users.noreply.github.com>
2022-08-26 06:36:05 +0000

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,64 @@
+import Link from 'next/link'
+  
+    <nav>
+      <div>
+        <ul>
+          <li>동아리 <span>▼</span>
+            <ul>
+              <li>
+                <Link href="">
+                  <a>임원진</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="">
+                  <a>동아리 소개</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="">
+                  <a>동아리 규칙</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="">
+                  <a>회의록</a>
+               </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link href="">
+              <a>프로젝트</a>
+            </Link></li>
+          <li>Education <span>▼</span>
+            <ul>
+              <li>
+                <Link href="">
+                  <a>Web</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="">
+                  <a>Mobile</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="">
+                  <a>Data Analysis</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="">
+                  <a>AI</a>
+                </Link>
+              </li>
+            </ul>
+          <li><Link href="">
+            <a>질문 게시판</a>
+            </Link>
+          </li>
+          </li>
+        </ul>
+      </div>
+   </nav> 
